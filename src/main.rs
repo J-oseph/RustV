a33c2f9608929c564dc0ca2b69540fb85db3b51d
Joseph <Joseph@email.com>
2023-02-26 14:12:46 -0800

    start of parsing program file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let mut program_file_string = fs::read_to_string("program.s")
+        .expect("Can't read file into string.");
+    
+    println!("{program_file_string}");
+    filter_program_file_string(&mut program_file_string)
+}
+
+fn filter_program_file_string(string:&String){
+    let mut temp: String = String::new();
+    for line in string.split('\n'){
+        let mut i = 0;
+        for token in line.split(' '){
+            println!("{i} {token} {:?}", token.as_bytes());
+            i += 1;
+        }
+    }
 }
